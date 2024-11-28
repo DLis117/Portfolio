0c3dbe9057446892d95895cfb0cbb4f07ec4b61f
DLis117 <domills117@gmail.com>
2024-11-28 13:10:42 +0100

    cards are now customized

--- js/ReactJS/cards/cardsProject/src/App.jsx
+++ js/ReactJS/cards/cardsProject/src/App.jsx
@@ -1,7 +1,16 @@
 import CardComponent from "./Card/Card.jsx"
-
+let mountainpic='/mountain.png';
+let fieldpic='/field.png';
+let autumnpic='/autumn.png';
+let sunsetpic='/sunset.png';
 function App() {
-  return (<><CardComponent/><CardComponent/><CardComponent/><CardComponent/></>);
+  return (<>
+            <CardComponent pic={mountainpic} desc='this card shows mountains' name='mountain card' key={0}/>
+            <CardComponent pic={fieldpic} desc='this card shows fields' name='fields card' key={1}/>
+            <CardComponent pic={autumnpic} desc='this card shows autumn' name='autumn card' key={2}/>
+            <CardComponent pic={sunsetpic} desc='this card shows sunset' name='sunset card' key={3}/>
+         </>
+  );
 }
 
 export default App
--- js/ReactJS/cards/cardsProject/src/Card/Card.jsx
+++ js/ReactJS/cards/cardsProject/src/Card/Card.jsx
@@ -1,16 +1,25 @@
 import styles from  '../Card/Card.module.css';
-function CardComponent()
+
+function CardComponent(props)
 {
+
     return(<div className={styles.card}>
             <div className={styles.imageContainer}>
-                <img src='/mountain.png'/>
+                <img src={props.pic}/>
             </div>
             <div className={styles.descriptionContainer}>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec tempor nulla. Cras et purus sit </p>
+                <p>{props.desc}</p>
             </div>
             <div className={styles.nameContainer}>
-                <h1>lorem ipsum</h1>
+                <h1>{props.name}</h1>
             </div>
-        </div>)
+        </div>);
 }
+
+CardComponent.defaultProps=
+{
+    name:'noname',
+    desc: 'nodesc',
+}
+
 export default CardComponent;
