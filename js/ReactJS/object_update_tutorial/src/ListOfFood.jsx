348ea7edcdef932b362b7dacf27f0fbbf89cbbd1
DLis117 <domills117@gmail.com>
2024-12-04 23:27:26 +0100

    added interactive list of items

--- js/ReactJS/object_update_tutorial/src/ListOfFood.jsx
+++ js/ReactJS/object_update_tutorial/src/ListOfFood.jsx
@@ -15,14 +15,20 @@ function ListOfFood()
         setList(prevList=>[...prevList,listelement]);
         setListElement("");
     }
+    function removeElement(index)
+    {
+        setList(list.filter((_,y)=>y!==index))
+            
+    }
 
     return (<>
                 <h1>add elements to the list</h1>
+                <p>if you want to delete an element just click on it</p>
                 <ul>
-                    {list.map((x,y)=><li key={y}>added element: {x} with index {y}</li>)}
+                    {list.map((x,y)=><li key={y} onClick={()=>removeElement(y)}>added element: {x} with index {y}</li>)}
                 </ul>
                 <input type="text" value={listelement} onChange={updateListElement}></input>
-                <button onClick={updateList}>add food to the list</button>
+                <button onClick={updateList}>add element to the list</button>
             </>)
 }
 export default ListOfFood;
