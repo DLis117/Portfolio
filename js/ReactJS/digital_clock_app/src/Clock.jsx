574d436e5647021cb1ad59a9f09991e7372d1a38
DLis117 <domills117@gmail.com>
2024-12-11 19:02:11 +0100

    digital clock app to train useEffect react hook

---
+++ js/ReactJS/digital_clock_app/src/Clock.jsx
@@ -0,0 +1,38 @@
+import { useEffect,useState } from "react";
+// import { useState } from "react"
+
+function Clock()
+{
+    let [date,setDate] =useState(new Date());
+
+    function formatTime(x)
+    {
+        let time="";
+        
+        if(x<10)
+        {
+            time+="0";
+        }
+       return time+=x.toString();
+    }
+    
+    
+    function updateClock()
+    {
+        setDate(new Date());
+    }
+
+    useEffect(()=>
+        {
+            const myInterval = setInterval(updateClock,1000);
+            return () => clearInterval(myInterval);
+            
+        });
+    return (<>
+                <div id="clock-container">
+                    <h1>{formatTime(date.getHours())}:{formatTime(date.getMinutes())}:{formatTime(date.getSeconds())}</h1>
+                </div>
+           </>)
+}
+
+export default Clock
