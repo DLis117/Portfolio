5f43510368ef28245f19dd20f77c847d2ec2a693
DLis117 <domills117@gmail.com>
2024-12-15 22:23:08 +0100

    accordion like list training

---
+++ js/ReactJS/tutorial_stuff/src/AccordionComponent.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function AccordionComponent(props)
+{
+    let [listOfStuff,setListOfStuff]=useState(props.info);
+
+    let [multipleInfosToggled,setMultipleInfosToggled]=useState(false);
+
+    function handleMultipleInfosToggled()
+    {
+        setMultipleInfosToggled(m=>m=!m);
+    }
+    function handleShowAdditionalInfo(indx)
+    {
+        let newListOfStuff=[...listOfStuff];
+        if(multipleInfosToggled)
+        {
+            newListOfStuff.map((x,y)=>y!=indx?x.additionalInfoActivated=false:null)
+            newListOfStuff[indx].additionalInfoActivated=!newListOfStuff[indx].additionalInfoActivated;
+        }
+        else
+        {
+            newListOfStuff[indx].additionalInfoActivated=!newListOfStuff[indx].additionalInfoActivated;
+        }
+
+        setListOfStuff(newListOfStuff);
+        newListOfStuff=null;
+    }
+    return(
+    <>
+    <div className="accordion-list-box">
+        <div className="header">
+            <h1>this is simple accordion like List</h1>
+            <button onClick={handleMultipleInfosToggled}>{multipleInfosToggled?"click to activate multiple selection":"click to activate single selection"}</button>
+        </div>
+        <ul>
+            {listOfStuff.map((x,indx)=><li onClick={()=>handleShowAdditionalInfo(indx)} key={indx}>{x.info} 🔽 {x.additionalInfoActivated&&<h2>{x.additionalInfo}</h2>}</li>)}
+        </ul>
+    </div>
+    </>
+    )
+}
+export default AccordionComponent
--- js/ReactJS/tutorial_stuff/src/App.jsx
+++ js/ReactJS/tutorial_stuff/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AccordionComponent from './AccordionComponent'
+let info = [
+            {info: 'Mercury is the planet nearest to the Sun',
+             additionalInfo: "and the smallest planet in our solar system",
+             additionalInfoActivated: false},
+
+            {info: 'Venus is the second planet from the Sun',
+            additionalInfo: "and the sixth largest planet",
+            additionalInfoActivated: false},
+
+            {info: 'Earth – our home planet – is the third planet from the Sun',
+            additionalInfo: "and the fifth largest planet",
+            additionalInfoActivated: false,}
+    ]
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return(
+          <><AccordionComponent info={info}/></>
+        )
 }
 
 export default App
