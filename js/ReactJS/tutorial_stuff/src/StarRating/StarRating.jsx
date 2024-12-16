0f8ee9c12f9f3ef71b1d45319cee6ccb34d5ee40
DLis117 <domills117@gmail.com>
2024-12-16 01:56:56 +0100

    added hovering handler

--- js/ReactJS/tutorial_stuff/src/StarRating/StarRating.jsx
+++ js/ReactJS/tutorial_stuff/src/StarRating/StarRating.jsx
@@ -5,21 +5,37 @@ function StarRating()
     const howManyStars=5;
 
     const [rating,setRating]=useState(Array.from({ length: howManyStars }, () => ({x:'★',clicked:false})));
-    const [overallRating,setOverallRaing]=useState(0);
+    const [overallRating,setOverallRating]=useState(0);
+    const [clickedStarsAmount,setClickedStarsAmount]=useState(0);
 
-    function handleRating(indx)
+    function commonEvent(indx)
     {
         let newRating=[...rating];
         newRating.map((x,y)=>y<=indx?x.clicked=true:x.clicked=false);
         setRating(newRating);
-        setOverallRaing(indx+1);
+    }
+    function handleRating(indx)
+    {
+        commonEvent(indx);
+        setOverallRating(indx+1);
+        setClickedStarsAmount(indx+1);
+    }
+    function handleHoveronRating(indx)
+    {
+        commonEvent(indx);
+    }
+    function handleLeaveonRating()
+    {
+        let newRating=[...rating];
+        newRating.map((x,y)=>y<=clickedStarsAmount-1?x.clicked=true:x.clicked=false);
+        setRating(newRating);
     }
     
     return (
             <>
                 <div className={style.starRatingContainer}>
                     {
-                        rating.map((x,y)=><p onClick={()=>handleRating(y)} className={x.clicked?style.yellowStar:style.blackStar} key={y}>{x.x}</p>)
+                        rating.map((x,y)=><p onClick={()=>handleRating(y)} onMouseOver={()=>handleHoveronRating(y)} onMouseLeave={handleLeaveonRating} className={x.clicked?style.yellowStar:style.blackStar} key={y}>{x.x}</p>)
                     }
                     <h2>overall rating: {overallRating}/{howManyStars}</h2>
                 </div>
