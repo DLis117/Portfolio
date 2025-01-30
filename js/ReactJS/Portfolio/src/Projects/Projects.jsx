18cddcdc3bb0d3d6deaaef248189da0c0f64afb7
DLis117 <domills117@gmail.com>
2025-01-30 19:55:57 +0100

    projects technologies buttons

--- js/ReactJS/Portfolio/src/Projects/Projects.jsx
+++ js/ReactJS/Portfolio/src/Projects/Projects.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import style from './Projects.module.css'
 function Projects()
 {
+    let [technologies,setTechnologies]= useState([
+        {name: 'react',activated:false},{name: 'java',activated:false},
+        {name: 'Node.js',activated:false},{name: 'javax',activated:false},
+        {name: 'react Native',activated:false},{name: 'javaxx',activated:false},
+        {name: 'Arduino uno nano',activated:false},{name: 'javaxxx',activated:false},
+        {name: 'Arduino uno nano dlugi tekst',activated:false},{name: 'javaxxxx',activated:false},
+       ])
+
+    function handleActivation(y)
+    {
+        setTechnologies(()=>{
+            let newTechnologies = technologies.map((x,z)=>({...x,activated:z===y}));
+            return newTechnologies;
+        })
+        
+    }
     return( <>
                 <div className={style.projectsPage}>
-                
+                    <div className={style.technologiesBar}>
+                        {technologies.map((x,y)=><button key={y} onClick={()=>handleActivation(y)} className={style.technologyButton} style={x.activated?{background: `rgba(108, 45, 179, 0.674)`}:null}>{x.name}</button>)}
+                    </div>
 
                 </div>
             </>)
