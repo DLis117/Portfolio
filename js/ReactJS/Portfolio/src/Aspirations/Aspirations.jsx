b6a834737ea3db800ca5275b45e4d5230dde6740
DLis117 <domills117@gmail.com>
2025-01-30 14:58:59 +0100

    aspirations page done

--- js/ReactJS/Portfolio/src/Aspirations/Aspirations.jsx
+++ js/ReactJS/Portfolio/src/Aspirations/Aspirations.jsx
@@ -10,7 +10,7 @@ function Aspirations()
                             <h2>
                                 That’s why I have decided to take a new direction and become a developer. 
                                 <br/><br/>
-                                In the future, I aim to combine web and mobile development with embedded systems—building the hardware, programming it, and creating both a mobile app and a website to complement it.
+                                In the future, I aim to combine web and mobile development with embedded systems—building the IoT hardware, programming it, and creating both a mobile app and a website to complement it.
                                 <br/><br/>
                                 To begin my journey, I have chosen to start with React.js for front-end web development. Next, I will expand into React Native for mobile development, followed by Node.js for both. Meanwhile, I am also learning embedded systems.
                             </h2>
--- js/ReactJS/Portfolio/src/ProgressBar/ProgressBar.jsx
+++ js/ReactJS/Portfolio/src/ProgressBar/ProgressBar.jsx
@@ -2,28 +2,27 @@ import style from './ProgressBar.module.css'
 function ProgressBar()
 {
     let progressData=[  
-                        {item: 'react1',started: true,finished: true},
-                        {item: 'react2',started: true,finished: false},
-                        {item: 'react3',started: false,finished: false},
-                        {item: 'react4',started: false,finished: false},
-                        {item: 'react5',started: false,finished: false},
-                        {item: 'react5',started: false,finished: false},
-                        {item: 'react5',started: false,finished: false},
+                        {item: 'start',started: true,finished: true},
+                        {item: 'React.js',started: true,finished: false},
+                        {item: 'React_Native',started: false,finished: false},
+                        {item: 'Node.js',started: false,finished: false},
+                        {item: 'IoT/embedded',started: false,finished: false},
+                        {item: 'finish',started: false,finished: false},
                     ];
 
     let howManyComponents = progressData.length;
-    let widthOfProgressBar = 100;//%
+    let widthOfProgressBar = 90;//%
     let widthOfFlexComponent = Math.floor(widthOfProgressBar/howManyComponents);
-    console.log(widthOfFlexComponent);
     let ballSize = widthOfFlexComponent*1.9;
     let lineWidth = widthOfFlexComponent*9;
-    let height= 15;// vh
+    let height= 16;// vh
+    
 
 
     function returnDiv(x,y)
     {
-        let filledBall = <div className={style.filledBall} style={{width:`${ballSize}px`,height:`${ballSize}px`}}>✓</div>;
-        let emptyBall  = <div className={style.emptyBall}  style={{width:`${ballSize}px`,height:`${ballSize}px`}}></div>;
+        let filledBall = <div className={style.ballFlexComponent}><div className={style.filledBall} style={{width:`${ballSize}px`,height:`${ballSize}px`}}>✓</div><p>{x.item}</p></div>;
+        let emptyBall  = <div className={style.ballFlexComponent}><div className={style.emptyBall}  style={{width:`${ballSize}px`,height:`${ballSize}px`}}></div><p>{x.item}</p></div>;
         let filledLine = <div className={style.filledLine} style={{width:`${lineWidth}px`,height:`${ballSize/8}px`}}/>;
         let emptyLine  = <div className={style.emptyLine}  style={{width:`${lineWidth}px`,height:`${ballSize/8}px`}}/>;
         let halfLine   = <div className={style.halfLine}   style={{width:`${lineWidth}px`,height:`${ballSize/8}px`}}/>;
@@ -62,9 +61,11 @@ function ProgressBar()
         }
     }
     return(<>
-                <div className={style.ProgressBarContainer} style={{height: `${height}vh`}}>
-                    <p>progress:</p>
-                    {progressData.map((x,y)=><div className={style.flexComponent} style={{width: `${widthOfFlexComponent}%`,height: `${height*0.4}vh`}}>{returnDiv(x,y)}</div>)}
+                <div className={style.ProgressBarContainer}>
+                        <div className={style.progressArrowContainer} style={{width:`${widthOfFlexComponent*howManyComponents}%`,height:`5vh`}}><h3 className={style.progressArrow}>⬐ i am currently here</h3></div>
+                        <div className={style.all}>
+                            {progressData.map((x,y)=><div className={style.flexComponent} style={{width: `${widthOfFlexComponent}%`,height: `${height*0.4}vh`}}>{returnDiv(x,y)}</div>)}
+                        </div>
                 </div>
             </>)
 }
