fd69d9fb931ce2a2ab69d368956d8356d7984172
DLis117 <domills117@gmail.com>
2025-02-03 01:05:23 +0100

    project description block added

--- js/ReactJS/Portfolio/src/Projects/ReactJS/ReactJS.jsx
+++ js/ReactJS/Portfolio/src/Projects/ReactJS/ReactJS.jsx
@@ -17,6 +17,7 @@ import ThemedComponent from './LightDarkModeTheme/ThemedComponent.jsx';
 import PopupComponent from './PopupComponent/PopupComponent.jsx';
 import TicTacToe from './TicTacToe/TicTacToe.jsx';
 import TierList from './TierList/TierList.jsx';
+import { useState } from 'react';
 let mountainpic='/mountain.png';
 let fieldpic='/field.png';
 
@@ -41,25 +42,134 @@ additionalInfoActivated: false,}
 let urlForGallery=`https://picsum.photos/v2/list?page=${1}`;
 
 let tiers = [
-                    {tier:'wwwwwwww',color:'#949'},
-                    {tier:'WWWWWWWW',color:'#a0a0a0'},
-                    {tier:'123456789',color:'#343434'},
-                    {tier:'1234567890',color:'#00ffff'},
-                    {tier:'12345678901',color:'#00ffff'},
-                    {tier:'123456789012',color:'#ff00ff'}
+                    {tier:'S',color:'#f5ef42'},
+                    {tier:'A',color:'#b6f542'},
+                    {tier:'B',color:'#42f5b3'},
+                    {tier:'C',color:'#42d1f5'},
+                    {tier:'D',color:'#4248f5'},
+                    {tier:'F',color:'#9b42f5'}
                    ]
+
 let dataToBeTiered =['/public/autumn.png','/public/field.png','public/mountain.png','/public/sunset.png','public/vite.svg',
     '/public/autumn.png','/public/field.png','public/mountain.png','/public/sunset.png','public/vite.svg',
     '/public/autumn.png','/public/field.png','public/mountain.png','/public/sunset.png','public/vite.svg',
     '/public/autumn.png','/public/field.png','public/mountain.png','/public/sunset.png','public/vite.svg'
 ]
+
+let projectsData=   [
+                        [//this will be wrapped together
+                            [//elements of this project, its title, its description 
+                                <>
+                                    <CardComponent pic={mountainpic} desc='mountains' name='mountain card' id={0}/>
+                                    <CardComponent pic={fieldpic} desc='fields' name='fields card' id={1}/>
+                                    <CardComponent/>
+                                </>,'Card Components',"test description"
+                            ],
+                            [
+                                <Counter/>,"Counter","test description 2"
+                            ],
+                            [
+                                <Clock/>,"Clock","clock desc TEST"
+                            ]
+                        ],
+                        [//this will be wrapped together
+                            [//elements of this project, its title, its description 
+                                <>
+                                    <ShoppingForm element="omega constelation"  styles={watch1Style}/>
+                                    <ShoppingForm element="omega speedmaster"   styles={watch2Style}/>
+                                    <ShoppingForm element="rolex daytona"       styles={watch3Style}/>
+                                </>,"conditional rendering card components","conditional rendering card components description"
+                            ]
+                        ],
+                        [
+                            [
+                                <Stopwatch/>,"Stopwatch","Stopwatch descriptoin"
+                            ],
+                            [
+                                <ToDoList/>,"To Do List","to do desc"
+                            ]
+                        ],
+                        [
+                            [
+                                <AccordionComponent info={accordionComponentInfo}/>,'accordion like List','accordion like List desc'
+                            ],
+                            [
+                                <StarRating/>,"Star Rating", "xd"
+                            ],
+                            [
+                                <AdvancedStarRating/>,"Advanced Star Rating","Xd"
+                            ]
+                        ],
+                        [
+                            [
+                                <ApiDataGallery url={urlForGallery} limit={10}/>,'gallery from API data',"xd"
+                            ],
+                            [
+                                <AutocompleteData/>,'display only filtered data',"xd"
+                            ]
+                        ],
+                        [
+                            [
+                                <GithubUserFinder/>,'Github User Finder','xd'
+                            ],
+                            [
+                                <ThemedComponent/>,'theme which remembers state after refresh','xd'
+                            ],
+                            [
+                                <PopupComponent/>,'popup toggler','xd'
+                            ]
+                        ],
+                        [
+                            [
+                                <TicTacToe/>,'Tic Tac Toe','xd' 
+                            ],
+                            [  
+                                <TierList tiers={tiers} data={dataToBeTiered} label={`tier list`}/>,'Tier List' 
+                            ]
+                        ]
+                        
+                    ];
+
+
 function ReactJS()
 {
+    
+    let [displayProjectInfo,setDisplayProjectInfo] = useState();
+    let [whereToDisplayInfo,setWhereToDisplayInfo] = useState();
+
+    function handleShowDescription(y,q)
+    {
+        //y - index of wrapTogether
+        //q - index of project in wrapTogether
+        if(displayProjectInfo!=projectsData[y][q][2])
+        {
+            setDisplayProjectInfo(projectsData[y][q][2]);
+        }
+        setWhereToDisplayInfo(y);
+        
+    }
     return (<>
                 <div className={style.reactJsProjectsContainer}>
                     {/* if the there is more than one component, and there is resizeable component we want to wrap those in single row
                     to prevent unknown behaviour if it start to overlap next components and move them unintentionally*/}
-                    <div className={style.wrapTogether}>
+                    {projectsData.map((x,y)=>
+                        <>
+                            <div className={style.wrapTogether}>
+                                {x.map((z,q)=>
+                                    // <ProjectWrapper project={z[0]} description={z[1]}/>
+                                    <div className={style.projectWrapper} onClick={()=>handleShowDescription(y,q)}>
+                                        <div className={style.titleOfProjectContainer}>
+                                            <h1 className={style.titleOfProject}>{z[1]}</h1>
+                                        </div>
+                                        <div className={style.project}>
+                                            {z[0]}
+                                        </div>
+                                    </div>
+                                )}
+                            </div>{whereToDisplayInfo===y&&<Description text={displayProjectInfo}/>}
+                        </>)}
+
+                    {/* <div className={style.wrapTogether}>
                         <ProjectWrapper project={<>
                                                     <CardComponent pic={mountainpic} desc='mountains' name='mountain card' id={0}/>
                                                     <CardComponent pic={fieldpic} desc='fields' name='fields card' id={1}/>
@@ -101,10 +211,10 @@ function ReactJS()
                     <div className={style.wrapTogether}>
                         <ProjectWrapper project={<TicTacToe/>} description='Tic Tac Toe'/>
                         <ProjectWrapper project={<TierList tiers={tiers} data={dataToBeTiered} label={`tier list`}/>} description='Tier List'/>    
-                    </div>
+                    </div> */}
                     
 
-                    <Description text={`smaple text`}/>
+                    
                 </div>
             </>)
 }
